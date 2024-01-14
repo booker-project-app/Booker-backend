c3b77171e5d1c3ddfa371af10369656b0eb47d1b
Jelena Adamović <105116909+adamoviccj@users.noreply.github.com>
2024-01-14 23:00:47 +0100

    minor changes

--- BookingApp/src/main/java/booker/BookingApp/controller/notifications/NotificationController.java
+++ BookingApp/src/main/java/booker/BookingApp/controller/notifications/NotificationController.java
@@ -2,21 +2,28 @@ package booker.BookingApp.controller.notifications;
 
 import booker.BookingApp.dto.notifications.NotificationDTO;
 import booker.BookingApp.service.implementation.NotificationService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/notifications")
 public class NotificationController {
     @Autowired
     private final NotificationService notificationService;
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
 
     public NotificationController(NotificationService notificationService) {
         this.notificationService = notificationService;
@@ -32,5 +39,65 @@ public class NotificationController {
     public ResponseEntity<ArrayList<NotificationDTO>> findAllPersonal(@PathVariable Long userId){
         ArrayList<NotificationDTO> personalNotifications = notificationService.findAllForUser(userId);
         return new ResponseEntity<>(personalNotifications, HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value="/sendNotificationRest", method = RequestMethod.POST)
+    public ResponseEntity<?> sendMessage(@RequestBody Map<String, String> message) {
+        if (message.containsKey("message")) {
+            if (message.containsKey("toId") && message.get("toId") != null && !message.get("toId").equals("")) {
+                this.simpMessagingTemplate.convertAndSend("/socket-publisher/" + message.get("toId"), message);
+                this.simpMessagingTemplate.convertAndSend("/socket-publisher/" + message.get("fromId"), message);
+            } else {
+                this.simpMessagingTemplate.convertAndSend("/socket-publisher", message);
+            }
+            return new ResponseEntity<>(message, new HttpHeaders(), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    /*
+     * WebSockets endpoint
+     *
+     * Kao sto smo koristili @RequestMapping za RestController, @MessageMapping se koristi za websocket-e
+     *
+     * Poruka ce biti poslata svim klijentima koji su pretplatili na /socket-publisher topic,
+     * a poruka koja im se salje je messageConverted (simpMessagingTemplate.convertAndSend metoda).
+     *
+     * Na ovaj endpoint klijenti salju poruke, ruta na koju klijenti salju poruke je /send/message (parametar @MessageMapping anotacije)
+     *
+     */
+    @MessageMapping("/send/notification")
+    public Map<String, String> broadcastNotification(String message) {
+        Map<String, String> messageConverted = parseMessage(message);
+
+        if (messageConverted != null) {
+            if (messageConverted.containsKey("toId") && messageConverted.get("toId") != null
+                    && !messageConverted.get("toId").equals("")) {
+                this.simpMessagingTemplate.convertAndSend("/socket-publisher/" + messageConverted.get("toId"),
+                        messageConverted);
+                this.simpMessagingTemplate.convertAndSend("/socket-publisher/" + messageConverted.get("fromId"),
+                        messageConverted);
+            } else {
+                this.simpMessagingTemplate.convertAndSend("/socket-publisher", messageConverted);
+            }
+        }
+
+        return messageConverted;
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<String, String> parseMessage(String message) {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> retVal;
+
+        try {
+            retVal = mapper.readValue(message, Map.class); // parsiranje JSON stringa
+        } catch (IOException e) {
+            retVal = null;
+        }
+
+        return retVal;
     }
 }
